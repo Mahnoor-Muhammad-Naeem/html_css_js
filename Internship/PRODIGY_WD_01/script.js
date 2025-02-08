45dc0927ef575e2956e71ac20d48263a4efb7c7b
Mahnoor-Muhammad-Naeem <166414336+Mahnoor-Muhammad-Naeem@users.noreply.github.com>
2025-02-08 11:29:30 +0500

    Add files via upload

---
+++ Internship/PRODIGY_WD_01/script.js
@@ -0,0 +1,10 @@
+// JavaScript for changing navbar color on scroll
+window.addEventListener("scroll", function() {
+    var navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+      navbar.style.backgroundColor = "#222";
+    } else {
+      navbar.style.backgroundColor = "#333";
+    }
+  });
+  
